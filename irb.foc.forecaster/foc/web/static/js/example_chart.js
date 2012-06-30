0cb904dc352d9919855261ad6e806923b79ab501
Matija Piškorec <matija.piskorec@gmail.com>
2012-06-30 20:35:54 +0200

    Adding scatter plot labels and tooltips.

--- irb.foc.forecaster/foc/web/static/js/example_chart.js
+++ irb.foc.forecaster/foc/web/static/js/example_chart.js
@@ -1,5 +1,8 @@
 
 var chart; // global
+var data; // DATA IS DEFINED AFTER AJAX REQUEST, SO DOES IT MAKE SENSE TO CALL IT WHILE DEFINING CHART? 
+
+var indicator = 'asdasdasd'
 
 /**
  * Request data from the server, add it to the graph and set a timeout to request again
@@ -10,6 +13,7 @@ function requestData() {
     $.ajax({
         url: '/getdata/',
         success: function(focData) {
+			data = focData;
             var series = chart.series[0],
                 shift = series.data.length > 20; // shift if the series is longer than 20
 			var country = focData[0];
@@ -21,7 +25,7 @@ function requestData() {
             // add the point
             //chart.series[0].addPoint(point, true, shift);
 			
-			
+			updatePlot();
             
             // call it again after one second
             //setTimeout(requestData, 1000);    
@@ -30,6 +34,13 @@ function requestData() {
     });
 }
 
+
+function updatePlot()
+{
+	chart.xAxis[0].axisTitle.attr({ text: data[0].x_ind.code.bold() }) ;
+	chart.yAxis[0].axisTitle.attr({ text: data[0].y_ind.code.bold() }) ;
+	// Alternative:  $(chart.yAxis[0].axisTitle.element).text('New Label');
+}
 
 $(document).ready(function() {
     chart = new Highcharts.Chart({
@@ -45,16 +56,27 @@ $(document).ready(function() {
         },
         xAxis: {
             tickPixelInterval: 150,
-            maxZoom: 50
+            maxZoom: 50,
+            title: {
+			    //margin: 80,
+				text: 'x'
+			},
         },
         yAxis: {
             minPadding: 0.2,
             maxPadding: 0.2,
             title: {
-                text: 'Value',
-                margin: 80
+                //margin: 80,
+				text: 'y'
             }
         },
+		tooltip: {
+			formatter: function() {
+					// This callback function is called after the chart is loaded and data defined!
+					//return 'x'.bold() + ':' + this.x.toFixed(2) + ', ' + 'y'.bold() + ': ' + this.y.toFixed(2);
+					return data[0].x_ind.code.bold() + ':' + this.x.toFixed(2) + '  ' + data[0].y_ind.code.bold() + ': ' + this.y.toFixed(2);
+			}
+		},
         series: [{
             name: 'Random data',
             data: []

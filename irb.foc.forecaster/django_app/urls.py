a0da0417254c317cd9f4c2e5f34dd310d1a9fdcc
Dražen Lučanin <kermit666@gmail.com>
2012-08-05 17:25:26 +0200

    removed root, as it doesn't work with it yet.

--- irb.foc.forecaster/django_app/urls.py
+++ irb.foc.forecaster/django_app/urls.py
@@ -17,7 +17,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     
-    url(r'', foc),
     url(r'^foc/', foc),
     url(r'^getdata/', get_data),
     
